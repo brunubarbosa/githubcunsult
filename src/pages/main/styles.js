f08cbce7767892c3eb8744bc42e896429c4fb335
Bruno <brunopjsw@gmail.com>
2019-07-07 21:45:46 -0300

    point 1

--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -11,23 +11,7 @@ const rotate = keyframes`
 `;
 
 
-export const Container = styled.div`
-    max-width: 700px;
-    background: #fff;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-    padding: 30px;
-    margin: 80px auto;
 
-    h1 {
-        font-size: 20px;
-        display: flex;
-        align-items: center;
-
-        svg {
-            margin-right: 10px;
-        }
-    }
-`;
 
 export const Form = styled.form`
     margin-top: 30px;
