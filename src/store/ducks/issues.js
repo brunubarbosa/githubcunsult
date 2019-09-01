94a94cd63bfbf0069a67cd246bdd83f86a38feff
Bruno <brunopjsw@gmail.com>
2019-09-01 20:39:56 -0300

    indent

--- src/store/ducks/issues.js
+++ src/store/ducks/issues.js
@@ -9,7 +9,13 @@ const INITIAL_STATE = []
 export default function issues(state = INITIAL_STATE, action) {
     switch(action.type) {
         case Types.EXPAND_ISSUE:
-            return [...state.map((issue, index) => index === action.payload.issueIndex ? {...issue, isExpanded: !issue.isExpanded} : {...issue, isExpanded: false} )]
+            return [
+                ...state.map((issue, index) => (
+                    index === action.payload.issueIndex ?
+                    {...issue, isExpanded: !issue.isExpanded} :
+                    {...issue, isExpanded: false}
+                ) )
+            ]
         case Types.SET_ITEMS:
             return [...action.payload.items]
         default:
